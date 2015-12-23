d3139b69aa5b2b2a8944293f68954b6dedeba160
will <will@willlam.net>
2015-12-23 08:03:54 -0500

    Initial Commit

---
+++ funfacts1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  funfacts1
+//
+//  Created by William Lam on 2015-12-23.
+//  Copyright © 2015 William Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ funfacts1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  funfacts1
+//
+//  Created by William Lam on 2015-12-23.
+//  Copyright © 2015 William Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
